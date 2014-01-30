7f5347d49e1536df44692575ac9a5f3aa7355116
Samir Undavia <samir1@brandeis.edu>
2014-01-30 14:41:18 -0500

    added methods

--- maze.rb
+++ maze.rb
@@ -1,2 +1,29 @@
+# Author: Samir Undavia
+
 class Maze
+
+	def initialize n,m
+		
+	end
+
+	def load arg
+
+	end
+
+	def display
+
+	end
+
+	def solve begX, begY, endX, endY
+
+	end
+
+	def trace begX, begY, endX, endY
+
+	end
+
+	def redesign
+
+	end
+
 end
